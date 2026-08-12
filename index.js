e2ef522703a5749e1ba22894d38e90811cbc6aa9
Ibhimwhar <ibhimwhar@gmail.com>
2026-08-12 15:31:57 -0400

    08-12-2026

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "https://think-and-grow-rich-client.vercel.app",
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
